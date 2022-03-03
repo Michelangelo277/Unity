129a9bab35f47bb7c392facbf4cd5348edd652e3
brandonsheehan <68979179+brandonsheehan@users.noreply.github.com>
2022-03-03 12:08:19 -0800

    Add files via upload

---
+++ AIPath.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPath : MonoBehaviour
+{
+    public float walkSpeed;
+    public bool mustPatrol;
+    public bool mustTurn;
+
+    public RigidBody2D rb;
+    public Transform groundCheckPosition;
+    public LayerMask groundLayer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustPatrol = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustPatrol == true)
+        {
+            Patrol();
+        }
+    }
+
+    void FixedUpdate 
+    {
+         if (mustPatrol)
+         {
+             mustTurn = !Physics2D.OverlapCircle(groundCheckPosition.position, 0.1f, groundLayer);
+         }
+    }
+
+    void Patrol()
+    {
+        if (mustTurn)
+        {
+            Flip();
+        }
+         rb.velocity = new Vector2(walkSpeed*Time.fixedDelaTime, rb.velocity.y));
+
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+}
